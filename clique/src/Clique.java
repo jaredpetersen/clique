ad1291147b3436585c4f4bf61183cbbb6044c720
netwarior <netwarior@gmail.com>
2015-06-01 08:51:40 -0700

    worked on the code

--- clique/src/Clique.java
+++ clique/src/Clique.java
@@ -26,12 +26,21 @@ public class Clique
 		{
 			scanner = new Scanner(new FileReader(fileName)).useDelimiter("\n");
 		
+			String row[] = scanner.next().split("\\W+");
+				
+			matrix = new int[row.length][row.length];
+			
+			int index = 0;
+			
+			for(int i = 0; i< row.length; i++)
+				matrix[index][i] = Integer.parseInt(row[i]);
+
 			while(scanner.hasNext())
 			{
-				String row[] = scanner.next().split(" ");
-				
-				// Just print out what's in the scanner
-				System.out.println(row.length);
+				index++;
+				row = scanner.next().split("\\W+");
+				for(int i = 0; i< row.length; i++)
+					matrix[index][i] = Integer.parseInt(row[i]);
 			}
 			
 			scanner.close();
@@ -42,5 +51,23 @@ public class Clique
 			e.printStackTrace();
 			System.exit(1);
 		}
+		
+		//print what's in Matrix
+		for (int i = 0; i< matrix.length; i++){
+			for(int j = 0; j< matrix.length; j++)
+				System.out.print(matrix[i][j]);
+			System.out.print('\n');
+		}
+		
+		System.out.println('\n');
+		for(int i = 0; i< matrix.length; i++)
+			System.out.println("Degree of " + i + " = " + degree(i));
+	}
+	
+	public int degree(int index){
+		int deg = 0;
+		for(int i = 0; i< matrix.length; i++)
+			deg += matrix[index][i];
+		return deg;
 	}
 }
