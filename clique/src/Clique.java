f3bda5b8de007175501bbffbdc052175d06b721c
netwarior <netwarior@gmail.com>
2015-06-01 10:51:22 -0700

    implemented degree checking

--- clique/src/Clique.java
+++ clique/src/Clique.java
@@ -15,10 +15,57 @@ public class Clique
 	private Scanner scanner;
 	// 2D array used to the matrix
 	int matrix[][];
+	// degree array
+	int degree[];
 	
 	// Constructor
 	public Clique()
 	{
+		InitMatrix();
+		InitDegree();
+		
+		//printMatrix();
+		
+		//for(int i = 0; i< matrix.length; i++)
+		//	System.out.println("Degree of " + i + " = " + degree(i));
+		
+		for(int i = 0; i< matrix.length; i++)
+			if(numOfDegreeOrHigher(i+1) >= i+1)
+				System.out.println(numOfDegreeOrHigher(i+1) + 
+					" vertices have degree " + (i+1) + " or higher");
+	}
+	
+	public void InitDegree(){
+		degree = new int[matrix.length];
+		int deg = 0;
+		for(int i = 0; i< matrix.length; i++) {
+			deg = 0;
+			for(int j = 0; j < matrix.length; j++)
+				deg += matrix[i][j];
+			degree[i] = deg;
+		}
+	}
+	
+	public int numOfDegreeOrHigher(int deg)
+	{
+		int num = 0;
+		for(int i = 0; i < matrix.length; i++)
+			if(degree[i] >= deg)
+				num++;
+		return num;
+	}
+	
+	public void printMatrix()
+	{
+		//print what's in Matrix
+		for (int i = 0; i< matrix.length; i++){
+			for(int j = 0; j< matrix.length; j++)
+				System.out.print(matrix[i][j]);
+			System.out.print('\n');
+		}
+	}
+	
+	public void InitMatrix(){
 		// Grab the input file
 		fileName = "graph.txt";
 		
@@ -51,23 +98,5 @@ public class Clique
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
-		//print what's in Matrix
-		for (int i = 0; i< matrix.length; i++){
-			for(int j = 0; j< matrix.length; j++)
-				System.out.print(matrix[i][j]);
-			System.out.print('\n');
-		}
-		
-		System.out.println('\n');
-		for(int i = 0; i< matrix.length; i++)
-			System.out.println("Degree of " + i + " = " + degree(i));
-	}
-	
-	public int degree(int index){
-		int deg = 0;
-		for(int i = 0; i< matrix.length; i++)
-			deg += matrix[index][i];
-		return deg;
 	}
 }
