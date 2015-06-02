6fe4638f4161f9736928d1b5dc83ecaffb50a746
Jared Petersen <jpetersen11@wou.edu>
2015-06-02 01:35:52 -0700

    Cleaned up and documented code

--- clique/src/Clique.java
+++ clique/src/Clique.java
@@ -3,113 +3,51 @@ import java.io.FileReader;
 import java.util.Scanner;
 
 /**
- * 
- * @author jshin13wou, jaredpetersen
- *
+ * Clique Class
+ * Used to find the largest clique in the given graph
+ * @author EthanEiter, jshin13wou, jaredpetersen
  */
 public class Clique 
 {
-	// Input file name
-	private String fileName;
-	// File scanner
-	private Scanner scanner;
-	// 2D array used to the matrix
-	int matrix[][];
-	// degree array
-	int degree[];
+	private String fileName; // Input file name
+	private Scanner scanner; // File Scanner
+	int matrix[][]; 		 // 2D array used to store the adjacency matrix
+	int degree[]; 			 // degree array
 	
-	// Constructor
+	/**
+	 * Clique Constructor
+	 */
 	public Clique()
 	{
+		// Get the graph in the form of an adjacency matrix
 		InitMatrix();
+		// Get the degrees for each vertex
 		InitDegree();
 		
-		//printMatrix();
-		
-		//for(int i = 0; i< matrix.length; i++)
-		//	System.out.println("Degree of " + i + " = " + degree(i));
-		
-		for(int i = matrix.length; i >= 1; i--)
-			if(numOfDegreeOrHigher(i-1) >= i-1)
-				if(checkClique(i))
-					break;// this means we found one
-				//System.out.println(numOfDegreeOrHigher(i+1) + 
-				//	" vertices have degree " + (i+1) + " or higher");
-	}
-	
-	private void InitDegree(){
-		degree = new int[matrix.length];
-		int deg = 0;
-		for(int i = 0; i< matrix.length; i++) {
-			deg = 0;
-			for(int j = 0; j < matrix.length; j++)
-				deg += matrix[i][j];
-			degree[i] = deg;
-		}
-	}
-	
-	private int numOfDegreeOrHigher(int deg)
-	{
-		int num = 0;
-		for(int i = 0; i < matrix.length; i++)
-			if(degree[i] >= deg)
-				num++;
-		return num;
-	}
-	
-	private void printMatrix()
-	{
-		//print what's in Matrix
-		for (int i = 0; i< matrix.length; i++){
-			for(int j = 0; j< matrix.length; j++)
-				System.out.print(matrix[i][j]);
-			System.out.print('\n');
-		}
-	}
-	
-	// input to this method is the number of nodes in the clique
-	private boolean checkClique(int size) {
-		System.out.println("checking for a clique of size " + size + "...\n");
-		int nodes[] = new int[numOfDegreeOrHigher(size-1)];
-		
-		int count = 0;
-		for(int i = 0; i< degree.length; i++)
-			if(degree[i] >= size-1){
-				nodes[count] = i;
-				count++;
+		// Begin solving the problem
+		// Iterate over the matrix, start at the highest clique size
+		for (int i = matrix.length; i >= 1; i--)
+		{	
+			if (numOfDegreeOrHigher(i-1) >= i-1)
+			{
+				// Check for a complete clique of size i
+				if (checkClique(i))
+				{
+					// Found the clique, stop looking
+					break;
+				}
 			}
-		
-		return combination(nodes, size);
-		
-		//return checkCurrClique(nodes, size);
+		}
 	}
 	
-	private boolean checkCurrClique(int[] nodes, int size)
+	/**
+	 * Set a 2D array that matches the input file
+	 */
+	@SuppressWarnings("resource")
+	private void InitMatrix()
 	{
-		boolean failed = false;
-		for(int i = 0; i < nodes.length-1; i++) // the last node NEVER needs to be checked
-		{
-			for (int j = i+1; j < nodes.length-1; j++)
-				if(matrix[nodes[i]][nodes[j]] == 0)
-					failed = true;
-			if(failed)
-				break;
-		}
-		if(failed)
-			failed=false;
-		else{
-			System.out.println("A clique exists of size " + size);
-			System.out.print("Nodes included: " + (nodes[0]+1));
-			for(int i = 1; i < nodes.length; i++)
-				System.out.print(", " + (nodes[i]+1));
-			System.out.println('\n');
-			return true;
-		}
-		return false;
-	}
-	
-	private void InitMatrix(){
 		// Grab the input file
+		// File needs to be in the folder just above where the code is located
 		fileName = "graph.txt";
 		
 		try
@@ -136,31 +74,180 @@ public class Clique
 			scanner.close();
 		}
 		
-		catch(FileNotFoundException e)
+		catch (FileNotFoundException e)
 		{
-			e.printStackTrace();
+			// File does not exist
+			System.out.println("Oops! It looks like we couldn't find your input file.");
+			System.out.println("Please read the documentation for more information.");
+			
+			// Shut the program down with an error
 			System.exit(1);
 		}
 	}
 	
+	/**
+	 * Set an array of the degrees for each vertex
+	 */
+	private void InitDegree()
+	{
+		degree = new int[matrix.length];
+		int deg = 0;
+		for (int i = 0; i < matrix.length; i++) 
+		{
+			deg = 0;
+			for (int j = 0; j < matrix.length; j++)
+			{
+				deg += matrix[i][j];
+			}
+			degree[i] = deg;
+		}
+	}
+	
+	/**
+	 * Finds the number of vertices with the input degree or higher
+	 * @param deg The degree
+	 * @return The number of the vertices with the input degree or higher
+	 */
+	private int numOfDegreeOrHigher(int deg)
+	{
+		int num = 0;
+		for (int i = 0; i < matrix.length; i++)
+		{
+			if(degree[i] >= deg)
+			{
+				num++;
+			}
+		}
+		return num;
+	}
+	
+	/**
+	 * Prints the input matrix
+	 * Used for debugging
+	 */
+	@SuppressWarnings("unused")
+	private void printMatrix()
+	{
+		//print what's in Matrix
+		for (int i = 0; i < matrix.length; i++)
+		{
+			for(int j = 0; j < matrix.length; j++)
+			{
+				System.out.print(matrix[i][j]);
+			}
+			System.out.print('\n');
+		}
+	}
+	
+	/**
+	 * Check if there is a complete clique with the input size
+	 * @param size The number of nodes in the clique
+	 * @return True if there is a complete clique with the input size
+	 */
+	private boolean checkClique(int size) 
+	{
+		/* Nodes used to store the nodes that fit the degree requirements
+		   i.e. if we're looking for a clique of size 4, don't include nodes that only have 
+		   a degree of 1 because there's no way it could be in the complete clique */
+		int nodes[] = new int[numOfDegreeOrHigher(size-1)];
+		int count = 0;
+		// Get the previously discussed nodes that fit the degree requirements
+		for (int i = 0; i < degree.length; i++)
+		{
+			if (degree[i] >= size-1)
+			{
+				nodes[count] = i;
+				count++;
+			}
+		}
+		
+		// Helper method called, used to check all of the combinations of the nodes array
+		// Uses brute force, but cuts down on the number of nodes that need to be checked
+		return combination(nodes, size);
+	}
+	
+	/**
+	 * 
+	 * @param nodes
+	 * @param size
+	 * @return
+	 */
+	private boolean checkCurrClique(int[] nodes, int size)
+	{
+		boolean failed = false;
+		for (int i = 0; i < nodes.length-1; i++) // the last node NEVER needs to be checked
+		{
+			for (int j = i+1; j < nodes.length-1; j++)
+			{
+				if (matrix[nodes[i]][nodes[j]] == 0)
+				{
+					failed = true;
+				}
+			}
+			if (failed)
+			{
+				break;
+			}
+		}
+		
+		if (failed)
+		{
+			failed=false;
+		}
+		else
+		{
+			// Found the largest complete clique
+			System.out.println("The largest clique is of size " + size);
+			System.out.print("Nodes included: " + (nodes[0]+1));
+			for(int i = 1; i < nodes.length; i++)
+			{
+				System.out.print(", " + (nodes[i]+1));
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param arr
+	 * @param r
+	 * @return
+	 */
 	private boolean combination(int[] arr, int r)
 	{
 		int[] res = new int[r];
 		return combine(arr, res, 0, 0, r);
 	}
 	
+	/**
+	 * 
+	 * @param arr
+	 * @param res
+	 * @param currIndex
+	 * @param level
+	 * @param r
+	 * @return
+	 */
 	private boolean combine(int[] arr, int[] res, int currIndex, int level, int r) {
-        if(level == r){
+        if (level == r)
+        {
         	if(checkCurrClique(res, r))
+        	{
 				return true;
+        	}
         	return false;
         }
-        for (int i = currIndex; i < arr.length; i++) {
+        for (int i = currIndex; i < arr.length; i++) 
+        {
             res[level] = arr[i];
-            if(combine(arr, res, i+1, level+1, r))
+            if (combine(arr, res, i+1, level+1, r))
+            {
     			return true;
-            //way to avoid printing duplicates
-            if(i < arr.length-1 && arr[i] == arr[i+1]){
+            }
+            // Avoid printing duplicates
+            if (i < arr.length-1 && arr[i] == arr[i+1])
+            {
                 i++;
             }
         }
--- clique/src/Main.java
+++ clique/src/Main.java
@@ -1,7 +1,6 @@
 /**
  * Main Class
  * Initializes the program and manages all of the user interaction bits
- * 
  * @author jshin13wou, jaredpetersen
  *
  */
@@ -10,8 +9,23 @@ public class Main
 	// Main method
 	public static void main(String[] args) 
 	{
+		// Print out the intro message
+		printIntro();
+		
 		// Create a new clique
-		Clique clique = new Clique();
+		new Clique();
+	}
+	
+	/**
+	 * Print out the intro message to the application
+	 */
+	private static void printIntro()
+	{
+		System.out.println("********************************************************");
+		System.out.println("Largest Complete Clique Finder\n");
+		System.out.println("Created by Ethan Eiter, Joseph Shin, & Jared Petersen");
+		System.out.println("********************************************************\n");
+		System.out.println("Grabbing the graph.txt input file...\n");
 	}
 
 }
